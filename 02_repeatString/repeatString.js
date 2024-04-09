5ca98e8f49af277b1786766a8b1ecd2184265d90
Leigh <6755798+LeighAH@users.noreply.github.com>
2024-04-09 18:36:44 +0100

    Implement 01 and 02, pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterations) {
+    if(iterations < 0) return "ERROR";
+    
+    let repeatedString = "";
+    for(i = 0; i < iterations; i++) {
+        repeatedString = repeatedString.concat(string);
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
