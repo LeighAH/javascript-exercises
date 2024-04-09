7b1492b2678417fbd07c4ebd3ffc1144c8ae893a
Leigh <6755798+LeighAH@users.noreply.github.com>
2024-04-10 00:10:36 +0100

    Implement 03 and 04, pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let chars = string.split("");
+    chars = chars.reverse();
+    return chars.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let argsArray = Array.from(arguments);
+    for(i = 1; i < argsArray.length; i++)
+    {
+        for(j = 0; j < arr.length; j++) 
+        {
+            if(argsArray[i] === arr[j])
+            {
+                arr[j] = undefined;
+            }
+        }
+    }
 
+    let outputArray = [];
+    for(i = 0; i < arr.length; i++)
+    {
+        if(arr[i] != undefined)
+        {
+            outputArray.push(arr[i]);
+        }
+    }
+
+    return outputArray;
 };
 
 // Do not edit below this line
