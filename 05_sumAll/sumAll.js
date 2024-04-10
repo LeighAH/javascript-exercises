ccc5591fe20bd89245b060ede87fe28bef528884
Leigh <6755798+LeighAH@users.noreply.github.com>
2024-04-10 18:30:09 +0100

    Implement 06 and 07, pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(typeof(start) != "number" || typeof(end) != "number" || start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for(i = (start < end ? start: end); i <= (start < end ? end : start); i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round((temp - 32.0) * 5.0 / 9.0 * 10.0) / 10.0;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(((temp * 9.0 / 5.0) + 32) * 10.0) / 10.0;
 };
 
 // Do not edit below this line
